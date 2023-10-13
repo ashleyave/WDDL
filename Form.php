f924c1c4098bc23d249745b2d448350f450ba7c2
Ashley A. Avecilla <91178071+ashleyave@users.noreply.github.com>
2023-10-13 15:41:51 +0800

    Add files via upload

---
+++ Form.php
@@ -0,0 +1,302 @@
+<script src="https://cdn03.jotfor.ms/static/prototype.forms.js?3.3.46412" type="text/javascript"></script>
+<script src="https://cdn01.jotfor.ms/static/jotform.forms.js?3.3.46412" type="text/javascript"></script>
+<script src="https://cdn02.jotfor.ms/js/vendor/jquery-1.8.0.min.js?v=3.3.46412" type="text/javascript"></script>
+<script defer src="https://cdn03.jotfor.ms/js/vendor/maskedinput.min.js?v=3.3.46412" type="text/javascript"></script>
+<script defer src="https://cdn01.jotfor.ms/js/vendor/jquery.maskedinput.min.js?v=3.3.46412" type="text/javascript"></script>
+<script defer src="https://cdn02.jotfor.ms//js/punycode-1.4.1.min.js"></script>
+<script src="https://cdn03.jotfor.ms/js/payments/validategateways.js?v=3.3.46412" type="text/javascript"></script>
+<script src="https://cdn01.jotfor.ms/s/umd/2ff6ca2ed7a/for-form-branding-footer.js?v=3.3.46412" type="text/javascript" defer></script>
+<script type="text/javascript">	JotForm.newDefaultTheme = true;
+	JotForm.extendsNewTheme = false;
+	JotForm.singleProduct = false;
+	JotForm.newPaymentUIForNewCreatedForms = false;
+	JotForm.texts = {"confirmEmail":"E-mail does not match","pleaseWait":"Please wait...","validateEmail":"You need to validate this e-mail","confirmClearForm":"Are you sure you want to clear the form","lessThan":"Your score should be less than or equal to","incompleteFields":"There are incomplete required fields. Please complete them.","required":"This field is required.","requireOne":"At least one field required.","requireEveryRow":"Every row is required.","requireEveryCell":"Every cell is required.","email":"Enter a valid e-mail address","alphabetic":"This field can only contain letters","numeric":"This field can only contain numeric values","alphanumeric":"This field can only contain letters and numbers.","cyrillic":"This field can only contain cyrillic characters","url":"This field can only contain a valid URL","currency":"This field can only contain currency values.","fillMask":"Field value must fill mask.","uploadExtensions":"You can only upload following files:","noUploadExtensions":"File has no extension file type (e.g. .txt, .png, .jpeg)","uploadFilesize":"File size cannot be bigger than:","uploadFilesizemin":"File size cannot be smaller than:","gradingScoreError":"Score total should only be less than or equal to","inputCarretErrorA":"Input should not be less than the minimum value:","inputCarretErrorB":"Input should not be greater than the maximum value:","maxDigitsError":"The maximum digits allowed is","minCharactersError":"The number of characters should not be less than the minimum value:","freeEmailError":"Free email accounts are not allowed","minSelectionsError":"The minimum required number of selections is ","maxSelectionsError":"The maximum number of selections allowed is ","pastDatesDisallowed":"Date must not be in the past.","dateLimited":"This date is unavailable.","dateInvalid":"This date is not valid. The date format is {format}","dateInvalidSeparate":"This date is not valid. Enter a valid {element}.","ageVerificationError":"You must be older than {minAge} years old to submit this form.","multipleFileUploads_typeError":"{file} has invalid extension. Only {extensions} are allowed.","multipleFileUploads_sizeError":"{file} is too large, maximum file size is {sizeLimit}.","multipleFileUploads_minSizeError":"{file} is too small, minimum file size is {minSizeLimit}.","multipleFileUploads_emptyError":"{file} is empty, please select files again without it.","multipleFileUploads_uploadFailed":"File upload failed, please remove it and upload the file again.","multipleFileUploads_onLeave":"The files are being uploaded, if you leave now the upload will be cancelled.","multipleFileUploads_fileLimitError":"Only {fileLimit} file uploads allowed.","dragAndDropFilesHere_infoMessage":"Drag and drop files here","chooseAFile_infoMessage":"Choose a file","maxFileSize_infoMessage":"Max. file size","generalError":"There are errors on the form. Please fix them before continuing.","generalPageError":"There are errors on this page. Please fix them before continuing.","wordLimitError":"Too many words. The limit is","wordMinLimitError":"Too few words.  The minimum is","characterLimitError":"Too many Characters.  The limit is","characterMinLimitError":"Too few characters. The minimum is","ccInvalidNumber":"Credit Card Number is invalid.","ccInvalidCVC":"CVC number is invalid.","ccInvalidExpireDate":"Expire date is invalid.","ccInvalidExpireMonth":"Expiration month is invalid.","ccInvalidExpireYear":"Expiration year is invalid.","ccMissingDetails":"Please fill up the credit card details.","ccMissingProduct":"Please select at least one product.","ccMissingDonation":"Please enter numeric values for donation amount.","disallowDecimals":"Please enter a whole number.","restrictedDomain":"This domain is not allowed","ccDonationMinLimitError":"Minimum amount is {minAmount} {currency}","requiredLegend":"All fields marked with * are required and must be filled.","geoPermissionTitle":"Permission Denied","geoPermissionDesc":"Check your browser's privacy settings.","geoNotAvailableTitle":"Position Unavailable","geoNotAvailableDesc":"Location provider not available. Please enter the address manually.","geoTimeoutTitle":"Timeout","geoTimeoutDesc":"Please check your internet connection and try again.","appointmentSelected":"You’ve selected {time} on {date}","noSlotsAvailable":"No slots available","slotUnavailable":"{time} on {date} has been selected is unavailable. Please select another slot.","multipleError":"There are {count} errors on this page. Please correct them before moving on.","oneError":"There is {count} error on this page. Please correct it before moving on.","doneMessage":"Well done! All errors are fixed.","doneButton":"Done","reviewSubmitText":"Review and Submit","nextButtonText":"Next","prevButtonText":"Previous","seeErrorsButton":"See Errors","notEnoughStock":"Not enough stock for the current selection","notEnoughStock_remainedItems":"Not enough stock for the current selection ({count} items left)","soldOut":"Sold Out","justSoldOut":"Just Sold Out","selectionSoldOut":"Selection Sold Out","subProductItemsLeft":"({count} items left)","startButtonText":"START","submitButtonText":"Submit","submissionLimit":"Sorry! Only one entry is allowed. Multiple submissions are disabled for this form.","reviewBackText":"Back to Form","seeAllText":"See All","progressMiddleText":"of","fieldError":"field has an error.","error":"Error"};
+	JotForm.newPaymentUI = true;
+
+   JotForm.setConditions([{"action":[{"field":"9","visibility":"Show","id":"action_0_1579704830003"}],"id":"1579704830003","index":"0","link":"Any","priority":"0","terms":[{"field":"8","operator":"equals","value":"Other"}],"type":"field"}]);	JotForm.submitError="jumpToFirstError";
+	window.addEventListener('DOMContentLoaded',function(){window.brandingFooter.init({"formID":232850913085457,"campaign":"powered_by_jotform_le","isCardForm":false,"isLegacyForm":true})});	JotForm.isFullSource = true;
+
+	JotForm.init(function(){
+	/*INIT-START*/
+      JotForm.setPhoneMaskingValidator( 'input_5_full', '\u0028\u0023\u0023\u0023\u0029 \u0023\u0023\u0023\u002d\u0023\u0023\u0023\u0023' );
+if (window.JotForm && JotForm.accessible) $('input_9').setAttribute('tabindex',0);
+if (window.JotForm && JotForm.accessible) $('input_11').setAttribute('tabindex',0);
+if (window.JotForm && JotForm.accessible) $('input_12').setAttribute('tabindex',0);
+	/*INIT-END*/
+	});
+
+   setTimeout(function() {
+JotForm.paymentExtrasOnTheFly([null,{"name":"clickTo","qid":"1","text":"New Customer Registration Form","type":"control_head"},null,{"name":"fullName3","qid":"3","text":"Full Name","type":"control_fullname"},{"name":"address4","qid":"4","text":"Address","type":"control_address"},{"name":"phoneNumber5","qid":"5","text":"Phone Number","type":"control_phone"},{"name":"email6","qid":"6","subLabel":"example@example.com","text":"E-mail","type":"control_email"},null,{"name":"howDid8","qid":"8","text":"How did you hear about us?","type":"control_dropdown"},{"name":"pleaseSpecify","qid":"9","text":"Please Specify","type":"control_textbox"},null,{"name":"feedbackAbout11","qid":"11","text":"Feedback about us:","type":"control_textarea"},{"name":"suggestionsIf","qid":"12","text":"Suggestions if any for further improvement:","type":"control_textarea"},null,{"name":"pleaseGive14","qid":"14","text":"Please give reference of any two people whom you feel:","type":"control_matrix"},{"name":"willYou15","qid":"15","text":"Will you be willing to recommend us?","type":"control_radio"},{"name":"divider","qid":"16","text":"Divider","type":"control_divider"},null,{"name":"input18","qid":"18","text":"\nACME GLOBAL COMPANY\n123 Maple Street, Houston, TX, 77002\n\nexample@example.com\nwww.example.com\n(123) 1234567\n\n","type":"control_text"},null,null,{"name":"bild","qid":"21","text":"acmeglobal-black.63b2c588afe320.84115372","type":"control_image"},null,null,null,{"name":"submit","qid":"25","text":"Submit","type":"control_button"},null,{"name":"input27","qid":"27","text":"Customer Details:\n ","type":"control_text"}]);}, 20); 
+</script>
+<link type="text/css" rel="stylesheet" href="https://cdn01.jotfor.ms/themes/CSS/form-common.css?v=7cca7cc
+"/>
+<style type="text/css">@media print{.form-section{display:inline!important}.form-pagebreak{display:none!important}.form-section-closed{height:auto!important}.page-section{position:initial!important}}</style>
+<link type="text/css" rel="stylesheet" href="https://cdn02.jotfor.ms/themes/CSS/5e6b428acc8c4e222d1beb91.css?v=3.3.46412&themeRevisionID=5eb3b4ae85bd2e1e2966db96"/>
+<link type="text/css" rel="stylesheet" href="https://cdn03.jotfor.ms/css/styles/payment/payment_styles.css?3.3.46412" />
+<link type="text/css" rel="stylesheet" href="https://cdn01.jotfor.ms/css/styles/payment/payment_feature.css?3.3.46412" />
+<link type="text/css" rel="stylesheet" href="https://cdn02.jotfor.ms/stylebuilder/donationBox.css?v=3.3.46412">
+<style type="text/css" id="form-designer-style">
+    /* Injected CSS Code */
+/*PREFERENCES STYLE*/
+    .form-all {
+      font-family: Inter, sans-serif;
+    }
+  
+    .form-label.form-label-auto {
+      
+    display: block;
+    float: none;
+    text-align: left;
+    width: 100%;
+  
+    }
+  
+    .form-line {
+      margin-top: 12px 36px 12px 36px px;
+      margin-bottom: 12px 36px 12px 36px px;
+      padding-top: 0;
+      padding-bottom: 0;
+    }
+  
+    .form-all {
+      max-width: 752px;
+      width: 100%;
+    }
+  
+    .form-label.form-label-left,
+    .form-label.form-label-right,
+    .form-label.form-label-left.form-label-auto,
+    .form-label.form-label-right.form-label-auto {
+      width: 230px;
+    }
+  
+    .form-all {
+      font-size: 16px
+    }
+  
+    .supernova .form-all, .form-all {
+      background-color: #fff;
+    }
+  
+    .form-all {
+      color: #2C3345;
+    }
+    .form-header-group .form-header {
+      color: #2C3345;
+    }
+    .form-header-group .form-subHeader {
+      color: #2C3345;
+    }
+    .form-label-top,
+    .form-label-left,
+    .form-label-right,
+    .form-html,
+    .form-checkbox-item label,
+    .form-radio-item label,
+    span.FITB .qb-checkbox-label,
+    span.FITB .qb-radiobox-label,
+    span.FITB .form-radio label,
+    span.FITB .form-checkbox label,
+    [data-blotid][data-type=checkbox] [data-labelid],
+    [data-blotid][data-type=radiobox] [data-labelid],
+    span.FITB-inptCont[data-type=checkbox] label,
+    span.FITB-inptCont[data-type=radiobox] label {
+      color: #2C3345;
+    }
+    .form-sub-label {
+      color: #464d5f;
+    }
+  
+  .supernova {
+    background-color: #f3f3fe;
+  }
+  .supernova body {
+    background: transparent;
+  }
+  
+    .form-textbox,
+    .form-textarea,
+    .form-dropdown,
+    .form-radio-other-input,
+    .form-checkbox-other-input,
+    .form-captcha input,
+    .form-spinner input {
+      background-color: #FFFFFF;
+    }
+  
+      
+    .supernova {
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-attachment: fixed;
+      background-position: center top;
+    }
+
+      .supernova, #stage {
+        background-image: none;
+      }
+    
+      .form-all {
+        background-image: none;
+      }
+    .form-all:before { content: none; } .form-all { margin-top: 72px; }/*PREFERENCES STYLE*//*__INSPECT_SEPERATOR__*/.form-header-group.header-small {
+    padding: 20px 52px !important;
+    border-radius : 6px 6px 0 0;
+    border-top: 1px solid #eee;
+    margin: 0 -38px;
+    border-bottom: 0;
+}
+
+.form-header-group.header-small h3 {
+    text-transform : uppercase;
+    font-size : 26px;
+    font-weight : 400;
+    text-align : left;
+}
+    /* Injected CSS Code */
+</style>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $fullName = $_POST["q3_fullName3"]["first"] . " " . $_POST["q3_fullName3"]["last"];
+    $address = $_POST["q4_address4"]["addr_line1"] . " " . $_POST["q4_address4"]["addr_line2"];
+    $city = $_POST["q4_address4"]["city"];
+    $state = $_POST["q4_address4"]["state"];
+    $postal = $_POST["q4_address4"]["postal"];
+    $phoneNumber = $_POST["q5_phoneNumber5"]["full"];
+    $email = $_POST["q6_email6"];
+    $howDidYouHear = $_POST["q8_howDid8"];
+    $pleaseSpecify = isset($_POST["q9_pleaseSpecify"]) ? $_POST["q9_pleaseSpecify"] : "";
+    $feedback = $_POST["q11_feedbackAbout11"];
+    $suggestions = $_POST["q12_suggestionsIf"];
+    $recommendation = $_POST["q15_willYou15"];
+    $references = $_POST["q14_pleaseGive14"];
+
+    // Define a professional stylesheet with more styles
+    echo "<html>";
+    echo "<head>";
+    echo "<style>";
+    echo "body {
+        font-family: 'Arial', sans-serif;
+        background-color: #f0f0f0;
+        padding: 20px;
+    }";
+    echo ".container {
+        max-width: 800px;
+        margin: 0 auto;
+        background-color: #fff;
+        border: 1px solid #ccc;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+        padding: 20px;
+    }";
+    echo ".header {
+        text-align: center;
+    }";
+    echo ".content {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }";
+    echo ".section {
+        width: 48%;
+        padding: 10px;
+        margin: 5px;
+        background-color: #f9f9f9;
+        border: 1px solid #ccc;
+        box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+        border-radius: 5px;
+    }";
+    echo "h1 {
+        text-transform: uppercase;
+        font-size: 28px;
+        font-weight: bold;
+        color: #333;
+        margin-bottom: 20px;
+    }";
+    echo "p {
+        color: #555;
+        margin: 10px 0;
+        font-size: 18px;
+    }";
+    echo "strong {
+        font-weight: bold;
+    }";
+    echo ".recommendation {
+        font-size: 20px;
+        background-color: #ffc107;
+        padding: 5px;
+    }";
+    echo ".reference-list {
+        list-style: none;
+        padding: 0;
+    }";
+    echo ".reference-item {
+        margin: 5px 0;
+    }";
+    echo ".output-divider {
+        border-top: 1px solid #ccc;
+        margin-top: 20px;
+    }";
+    echo ".recommendation {
+        background-color: #ffc107;
+        padding: 10px;
+        border-radius: 5px;
+    }";
+    echo ".reference-list {
+        padding-left: 20px;
+    }";
+    echo ".reference-item {
+        margin: 10px 0;
+        border-left: 3px solid #ffc107;
+        padding-left: 10px;
+    }";
+    echo ".content:after {
+        content: '';
+        flex-basis: 100%;
+        height: 0;
+        clear: both;
+    }";
+    echo "</style>";
+    echo "</head>";
+    echo "<body>";
+    echo "<div class='container'>";
+    echo "<div class='header'>";
+    echo "<h1>Submitted Data</h1>";
+    echo "</div>";
+    echo "<div class='content'>";
+    echo "<div class='section'>";
+    echo "<p><strong>Full Name:</strong> $fullName</p>";
+    echo "<p><strong>Address:</strong> $address, $city, $state $postal</p>";
+    echo "<p><strong>Phone Number:</strong> $phoneNumber</p>";
+    echo "<p><strong>Email:</strong> $email</p>";
+    echo "<p><strong>How Did You Hear:</strong> $howDidYouHear</p>";
+    echo "<p><strong>Please Specify:</strong> $pleaseSpecify</p>";
+    echo "<p><strong>Feedback:</strong> $feedback</p>";
+    echo "<p><strong>Suggestions:</strong> $suggestions</p>";
+    echo "</div>";
+
+    echo "<div class='section'>";
+    echo "<p><strong>Recommendation:</strong></p>";
+    echo "<p class='recommendation'>$recommendation</p>";
+    if (!empty($references)) {
+        echo "<h2>References:</h2>";
+        echo "<ul class='reference-list'>";
+        foreach ($references as $reference) {
+            list($name, $address, $contact) = $reference;
+            echo "<li class='reference-item'><strong>Name:</strong> $name<br><strong>Address:</strong> $address<br><strong>Contact:</strong> $contact</li>";
+        }
+        echo "</ul>";
+    }
+    echo "</div>";
+    echo "</div>";
+    echo "<div class='output-divider'></div>";
+    echo "</div>"; // Close the container div
+    echo "</body>";
+    echo "</html>";
+}
+?>
+
+</form><script type="text/javascript">JotForm.ownerView=true;</script><script src="https://cdn.jotfor.ms//js/vendor/smoothscroll.min.js?v=3.3.46412"></script>
+<script src="https://cdn.jotfor.ms//js/errorNavigation.js?v=3.3.46412"></script>
+<script type="text/javascript">JotForm.isNewSACL=true;</script>
